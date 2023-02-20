a91473134728609ff1ed93bbbc6895c1c54ab482
Eniawealth <eniawealth224@gmail.com>
2023-02-20 11:53:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main- A C Programme that uses printf to give out result 
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+}
