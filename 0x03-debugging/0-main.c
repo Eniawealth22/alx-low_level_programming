e2e8d33b3a12b794afedce4b850ff274a7efce84
Eniawealth <eniawealth224@gmail.com>
2023-02-24 10:12:28 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
-    int zero = 0;
+	int i;
 
-    positive_or_negative(zero);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
-
